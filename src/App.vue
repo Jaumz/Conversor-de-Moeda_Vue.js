c9b238505798a011f63b6986a8b0ef179ab82625
Jo_ao <joaopedro.viana07@gmail.com>
2021-11-18 20:32:51 -0300

    App update

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,18 @@
 <template>
   <div id="app">
+    <h1>Conversor de Moedas</h1>
+    <div class="linha col-sm-12"> 
     <Conversor moeda-a="USD" moeda-b="BRL"/>
+    <Conversor moeda-a="BRL" moeda-b="USD"/>
+    </div>
+    <div class="linha col-sm-12"> 
+    <Conversor moeda-a="CAD" moeda-b="BRL"/>
+    <Conversor moeda-a="BRL" moeda-b="CAD"/>
+  </div>
+  <div class="linha col-sm-12"> 
+    <Conversor moeda-a="EUR" moeda-b="BRL"/>
+    <Conversor moeda-a="BRL" moeda-b="EUR"/>
+  </div>
   </div>
 </template>
 
@@ -22,7 +34,24 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #063463;
+  color: #51CC60;
   margin-top: 60px;
+  max-width: 900px;
+  margin: auto;
 }
+
+.linha{
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+  margin-top: 40px;
+}
+
+/* Extra small devices (phones, 600px and down) */
+@media only screen and (max-width: 600px) {
+    .col-sm-12 {
+      width: 100%;
+      margin-bottom: 20px;}
+}
+
 </style>
